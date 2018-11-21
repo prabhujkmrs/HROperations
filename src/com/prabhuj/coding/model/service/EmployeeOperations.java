f87b46c12bf1e5f9b7ce18389f31591504ff7175
pjeyakumar <pjeyakumar@and.digital>
2018-11-21 16:24:23 +0000

    minor refactoring

--- src/com/prabhuj/coding/model/service/EmployeeOperations.java
+++ src/com/prabhuj/coding/model/service/EmployeeOperations.java
@@ -3,9 +3,7 @@ package com.prabhuj.coding.model.service;
 import com.prabhuj.coding.Application;
 import com.prabhuj.coding.model.Employee;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class EmployeeOperations {
 
@@ -15,8 +13,8 @@ public class EmployeeOperations {
 
     public Map<String,Integer> total_Salary_Paid_Per_Department(List<Employee> employeeList){
         Map<String,Integer> result = new HashMap<>();
-        for (Employee emp: employeeList)
-        {
+        //java 8 foreach
+        employeeList.forEach(emp -> {
             if(! result.containsKey(emp.getDepartment().getDepartmentName())) {
                 result.put(emp.getDepartment().getDepartmentName(), emp.getSalary());
             }
@@ -24,13 +22,14 @@ public class EmployeeOperations {
                 int total_salary = result.get(emp.getDepartment().getDepartmentName())+emp.getSalary();
                 result.replace(emp.getDepartment().getDepartmentName(), total_salary );
             }
-        }
+        });
         return result;
     }
 
-    public Map.Entry<String, Integer> department_With_Max_No_Of_Employees(List<Employee> employeeList){
-        Map<String,Integer> result = new HashMap<>();
 
+    public String department_With_Max_No_Of_Employees(List<Employee> employeeList){
+        Map<String,Integer> result = new HashMap<>();
+        // normal for each loop
         for (Employee emp: employeeList) {
 
             if(! result.containsKey(emp.getDepartment().getDepartmentName())) {
@@ -42,17 +41,9 @@ public class EmployeeOperations {
             }
 
         }
+        // Java8 find the max value in map and return its key
+        return Collections.max(result.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
 
-        Map.Entry<String, Integer> maxEntry = null;
-
-        for (Map.Entry<String, Integer> entry : result.entrySet())
-        {
-            if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0)
-            {
-                maxEntry = entry;
-            }
-        }
-        return maxEntry;
     }
 
     public Map.Entry<String, Integer> department_Where_Total_Sal_Paid_Highest(List<Employee> employeeList){
@@ -69,6 +60,8 @@ public class EmployeeOperations {
                 result.replace(emp.getDepartment().getDepartmentName(), total_salary );
             }
         }
+
+        //java 7 finding max value in collection
         Map.Entry<String, Integer> maxEntry = null;
         for (Map.Entry<String, Integer> entry : result.entrySet())
         {
